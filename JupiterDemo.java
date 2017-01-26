d050bb4f3aeab0788c128ec0033d6b713094382f
lane.c.wagner <lane.c.wagner@gmail.com>
2017-01-26 09:28:18 -0700

    initMainMenu added and more fucntion headers

--- JupiterDemo.java
+++ JupiterDemo.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JupiterDemo extends javax.swing.JFrame {
-    public int stdTextBoxWidth = 100;
+    public int stdTextBoxWidth = 150;
     public int stdTextBoxHeight = 35;
+    public int bgColor = 0;
+    public int btnColor = 0;
+    public int innerMargins = 5;
 
     public JupiterDemo() {
         initComponents();
@@ -40,10 +43,10 @@ public class JupiterDemo extends javax.swing.JFrame {
         JPasswordField passField =  new JPasswordField();
         passField.setPreferredSize(new Dimension(stdTextBoxWidth, stdTextBoxHeight));
         getContentPane().add(passField, c);
-
-        //Create Login Button
+       
+        //Add new employee button
         c.gridy = 2;
-        JButton loginButton = new JButton("Login");
+        JButton loginButton = new JButton("Add New Employee");
         loginButton.setPreferredSize(new Dimension(stdTextBoxWidth, stdTextBoxHeight));
         getContentPane().add(loginButton, c);
         
@@ -53,14 +56,105 @@ public class JupiterDemo extends javax.swing.JFrame {
 
     //draws main menu page
     public void initMainMenu(){
-        //client search bar
-        //client search results field
-        //teller functions button
+    
+    //variables
+    JTextField cSearch = new JTextField();
+    JScrollPane jScrollPane1 = new JScrollPane();
+    JButton searchButton = new JButton();
+    JTextArea searchResults = new JTextArea();
+    JButton telButton = new JButton();
+    
+        //Client Search bar
+        cSearch = new JTextField();
+        cSearch.setText("Client Search");
+        cSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+               //when butttn is pressed
+           }
+        });
+        
+        //Search Button
+        searchButton = new JButton();
+        searchButton.setText("Search");
+        
+
+        //Teller Options Button
+        telButton = new javax.swing.JButton();
+        telButton.setText("Teller Options");
+        
+        
+        //Search Results Field
+        jScrollPane1 = new javax.swing.JScrollPane();
+        searchResults = new javax.swing.JTextArea();
+        searchResults.setColumns(20);
+        searchResults.setRows(5);
+        searchResults.setText("Search Results");
+        jScrollPane1.setViewportView(searchResults);
+        
+        //Set and initilize main menu jframe 
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(telButton, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
+                            .addComponent(cSearch))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(searchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 651, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(telButton, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(cSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(searchButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 482, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }
+    
+    //draw a client's screen
+    public void initClientScreen(){
+        //show client's personal information
+        //show clients account'sand loans basic info 
+    }
+    
+    //draw an account's screen
+    public void initAccountScreen(){
+        //show balance
+        //show recent transactions
+        //deposit button
+        //withdrawal button
+        //transfer button
+    }
+    
+    //draw a loan/card screen
+    public void initLoanScreen(){
+        //draw balance
+        //draw loan recent transactions
+        //draw payment button
+        //loan draw button (if it is a rotating loan)
     }
     
     //draw teller funtions screen
     public void initTellerFunctions(){
-        
+    //Change Employee Password Button
+    //Change Client Information Button 
+    //Reverse a Transaction Button
+    //Balance Drawer Button
     }
     
     //clear all objects from the screen
@@ -68,7 +162,7 @@ public class JupiterDemo extends javax.swing.JFrame {
         
     }
     
-    //don't touch
+    //Don't Touch
     public static void main(String args[]) {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
